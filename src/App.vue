e129b6eeda222ee2cc3e7b6170add0e0e41c09e4
Hector Diaz <3kthor3adward@gmail.com>
2020-06-03 10:37:05 -0500

    add login component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <Login></Login>
+  </v-app>
 </template>
 
 <script>
+import Login from './components/Auth/Login'
+
 export default {
   name: 'App',
   components: {
-    
+    Login
   }
 }
 </script>
---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="blue">
+            <v-toolbar-title>Login Form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+              <v-text-field prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn rounded dark color="indigo">Sign up</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn rounded dark color="primary">
+              Log in
+              <v-icon>keyboard_arrow_right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "loggin"
+};
+</script>
