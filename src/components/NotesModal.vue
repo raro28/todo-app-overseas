72847a58560bba783cf4721b73e23e1f0325f81e
Hector Diaz <3kthor3adward@gmail.com>
2020-06-30 10:06:12 -0500

    add tasks modal

---
+++ src/components/NotesModal.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="open" scrollable max-width="60%">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Task title</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-form>
+              <v-textarea
+                outlined
+                v-model="note"
+                label="Note"
+                placeholder="Start writing your note here"
+              ></v-textarea>
+              <v-btn color="success">Save</v-btn>
+            </v-form>
+          </v-container>
+          <v-list three-line subheader>
+            <v-subheader>Notes</v-subheader>
+            <v-list-item v-for="note in notes" v-bind:key="note.id">
+              <v-list-item-content>
+                <v-list-item-title>{{note.note}}</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                  <v-btn icon>
+                      <v-icon color="red">delete</v-icon>
+                  </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn text color="primary">Close</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn text dark color="red">Remove task</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "NotesModal",
+  data: function() {
+    return {
+      open: false,
+      note: "",
+      notes: [
+        {
+          id: 0,
+          note: "random note"
+        },
+        {
+          id: 2,
+          note: "random note"
+        },
+        {
+          id: 3,
+          note: "random note"
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,55 +1,59 @@
 <template>
-  <v-card style="height: 100%; overflow: hidden">
-    <v-toolbar dark color="blue">
-      <v-toolbar-title>Title of the list</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-list two-line subheader  style="height: calc(100vh - 164px); overflow-y: scroll;">
-      <task v-for="task in tasks" v-bind:key="task.id" v-bind:task="task"/>
-    </v-list>
-    <v-divider></v-divider>
-    <v-card-actions>
+  <div>
+    <v-card style="height: 100%; overflow: hidden">
+      <v-toolbar dark color="blue">
+        <v-toolbar-title>Title of the list</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-list two-line subheader style="height: calc(100vh - 164px); overflow-y: scroll;">
+        <task v-for="task in tasks" v-bind:key="task.id" v-bind:task="task" />
+      </v-list>
+      <v-divider></v-divider>
+      <v-card-actions>
         <v-layout>
-            <v-flex>
-                <new-task></new-task>
-            </v-flex>
+          <v-flex>
+            <new-task></new-task>
+          </v-flex>
         </v-layout>
-    </v-card-actions>
-  </v-card>
+      </v-card-actions>
+    </v-card>
+    <notes-modal />
+  </div>
 </template>
 
 <script>
 import Task from "./Task";
 import NewTask from "./NewTask";
+import NotesModal from "./NotesModal";
 
 export default {
   name: "Tasks",
-  components: { Task, NewTask },
+  components: { Task, NewTask, NotesModal },
   data: function() {
     return {
-        tasks: [
-            {
-                id: 0,
-                title: "task title",
-                subTitle: "asd as da d as da sd a d ad",
-                isComplete: true
-            },
-            {
-                id: 1,
-                title: "task title",
-                subTitle: "asd as da d as da sd a d ad",
-                isComplete: false
-            },
-            {
-                id: 3,
-                title: "task title",
-                subTitle: "asd as da d as da sd a d ad",
-                isComplete: true
-            }
-        ]
+      tasks: [
+        {
+          id: 0,
+          title: "task title",
+          subTitle: "asd as da d as da sd a d ad",
+          isComplete: true
+        },
+        {
+          id: 1,
+          title: "task title",
+          subTitle: "asd as da d as da sd a d ad",
+          isComplete: false
+        },
+        {
+          id: 3,
+          title: "task title",
+          subTitle: "asd as da d as da sd a d ad",
+          isComplete: true
+        }
+      ]
     };
   }
 };
